3c42a62b41e1940af6a791e448a334b5bde998a4
Konstantin Berengolts <dc.kokis@gmail.com>
2022-04-21 20:06:42 +0700

    Add files via upload

--- demodulation/main.py
+++ demodulation/main.py
@@ -22,13 +22,13 @@ def demod(S_t, fs, Um, fm, Uc, fc, L, size):
     sin = Uc * np.sin(2 * np.pi * fc * t)
     Re = S_t * cos
     Im = S_t * sin
-    Re = low_freq_filter(Re, fc, L)
-    Im = low_freq_filter(Im, fc, L)
+    Re = low_freq_filter(Re, fc*2, L)
+    Im = low_freq_filter(Im, fc*2, L)
     return np.array([complex(Re[i], Im[i]) for i in range(len(Re))])
 
 
-fm = 13
-fc = 13
+fm = 20
+fc = 20
 Uc = 1
 Um = 1
 L = 5
@@ -36,7 +36,7 @@ size = 1000
 fs = 20
 t = np.linspace(0, L, size)
 noise = np.array([(np.random.rand() - 0.5) for i in range(size)])
-S_t = np.sin(2 * np.pi * fs * t) + 2 * np.sin(2 * np.pi * (fs - 17) * t) + 0.5 * np.sin(2 * np.pi * (fs + 25) * t) + noise
+S_t = np.sin(2 * np.pi * fs * t) + 2 * np.sin(2 * np.pi * (fs - 15) * t) + 0.5 * np.sin(2 * np.pi * (fs + 20) * t) + noise
 res = demod(S_t, fc, Um, fm, Uc, fc, L, size)
 plt.subplot(4, 1, 1)
 plt.plot(t, S_t)
